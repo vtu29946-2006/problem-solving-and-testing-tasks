8648ffab7c8bc1b9dba0f7471cfd3975dd221d68
vtu29946-2006 <vtu29946@veltech.edu.in>
2026-08-03 12:03:49 +0530

    Add files via upload

---
+++ salary1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        if (!sc.hasNextInt()) {
+            return; // no input
+        }
+        int N = sc.nextInt();
+
+        List<Integer> salaries = new ArrayList<>();
+        for (int i = 0; i < N && sc.hasNextInt(); i++) {
+            salaries.add(sc.nextInt());
+        }
+
+        List<Integer> updatedSalaries = salaries.stream()
+                .map(salary -> (int) Math.round(salary * 1.10))
+                .collect(Collectors.toList());
+
+        for (int salary : updatedSalaries) {
+            System.out.print(salary + " ");
+        }
+
+        sc.close();
+    }
+}

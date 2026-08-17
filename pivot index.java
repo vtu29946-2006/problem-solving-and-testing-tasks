7709f1324e1d5a62d50d51b1a1b5e19b743f5171
vtu29946-2006 <vtu29946@veltech.edu.in>
2026-08-17 11:27:43 +0530

    Add comments for clarity in pivot index calculation
    Added comments to clarify the calculation of total sum and the process of finding the pivot index.

--- pivot index.java
+++ pivot index.java
@@ -2,14 +2,14 @@ class Solution {
     public int pivotIndex(int[] nums) {
         int totalSum = 0;
 
-        // Calculate total sum
+      
         for (int num : nums) {
             totalSum += num;
         }
 
         int leftSum = 0;
 
-        // Find pivot index
+       
         for (int i = 0; i < nums.length; i++) {
             int rightSum = totalSum - leftSum - nums[i];
 

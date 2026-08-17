6159a65af46cc53d7cf4d0a6d4e87961ca51c27b
vtu29946-2006 <vtu29946@veltech.edu.in>
2026-08-17 11:25:19 +0530

    Refactor frequencySort method in Solution class

--- week 3/solution(2).java
+++ week 3/solution(2).java
@@ -2,19 +2,18 @@ import java.util.*;
 
 class Solution {
     public String frequencySort(String s) {
-        // Step 1: Count frequency
+        
         Map<Character, Integer> freq = new HashMap<>();
         for (char c : s.toCharArray()) {
             freq.put(c, freq.getOrDefault(c, 0) + 1);
         }
 
-        // Step 2: Sort characters by frequency
+       
         PriorityQueue<Map.Entry<Character, Integer>> pq =
             new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
 
         pq.addAll(freq.entrySet());
 
-        // Step 3: Build result
         StringBuilder sb = new StringBuilder();
         while (!pq.isEmpty()) {
             Map.Entry<Character, Integer> entry = pq.poll();
@@ -28,7 +27,7 @@ class Solution {
         return sb.toString();
     }
 
-    // Quick test
+   
     public static void main(String[] args) {
         Solution sol = new Solution();
         System.out.println(sol.frequencySort("tree"));    // Output: "eert" or "eetr"
@@ -36,4 +35,3 @@ class Solution {
         System.out.println(sol.frequencySort("Aabb"));    // Output: "bbAa" or "bbaA"
     }
 }
-S
